7f5933ba002555d61a257078eb526608e5ca17ca
edm-r <edmondyopa22@gmail.com>
2024-11-05 00:31:20 +0100

    all

--- deleteTask.js
+++
@@ -1,7 +0,0 @@
-import { displayTasks } from './displayTasks.js';
-
-export function deleteTask(id) {
-    tasks = tasks.filter(task => task.id !== id);
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-    displayTasks();
-}
